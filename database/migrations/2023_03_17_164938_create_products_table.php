fc3a0382ab1e94209bb394c408e72267d8b43500
Emerson Solano <emerson-solano@hotmail.com>
2023-03-17 11:39:00 -0600

    add modelo Product con recursos y migracion

---
+++ database/migrations/2023_03_17_164938_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('categoria');
+            $table->string('nombre');
+            $table->float('costo_unitario');
+            $table->float('precio_unitario');
+            $table->float('en_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
